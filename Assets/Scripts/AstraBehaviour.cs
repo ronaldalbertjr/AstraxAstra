3dc8368831d6bfa40c7b88d25024a805326de42b
Ronald Albert <ronaldalbert1609@gmail.com>
2016-10-25 07:58:44 -0200

    AstraIsNowWalking
    Astra's walking animation was added to the game

--- Assets/Scripts/AstraBehaviour.cs
+++ Assets/Scripts/AstraBehaviour.cs
@@ -7,12 +7,22 @@ public class AstraBehaviour : MonoBehaviour
     float h;
 	[SerializeField] float speed;
     bool faceRight;
+    bool walkController;
+    Animator anim;
+    Rigidbody2D rb;
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
     void FixedUpdate()
     {
         w = Input.GetAxis("Horizontal");
         h = Input.GetAxis("Vertical");
 
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(w * speed, h * speed);
+        rb.velocity = new Vector2(w * speed, h * speed);
+
+        CheckWalkAnimation();
 
         if (w < 0 && !faceRight) Flip();
         else if (w > 0 && faceRight) Flip();
@@ -24,5 +34,10 @@ public class AstraBehaviour : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+    void CheckWalkAnimation()
+    {
+        walkController = (rb.velocity.x != 0 || rb.velocity.y != 0);
+        anim.SetBool("walking", walkController);
     }
 }
