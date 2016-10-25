de5e3d058dfce79449d9f4ca6b39ffaa2210d8a4
Ronald Albert <ronaldalbert1609@gmail.com>
2016-10-25 15:01:47 -0200

    FixedCreditsScene

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -24,7 +24,7 @@ public class PauseScript : MonoBehaviour
 
     public void OnReturnToMenuClick()
     {
-        Application.LoadLevel("NAVEIntroScene");
+        Application.LoadLevel(0);
     }
     public void OnExitClick()
     {
